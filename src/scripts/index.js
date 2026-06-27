01284fe97c031ca9067bc8b7a67b24713b0cb681
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-06-27 13:33:45 -0400

    feat: separated CSS into multiple stylesheets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,5 @@
 import "../stylesheets/custom-reset.css";
-import "../stylesheets/style.css";
+import "../stylesheets/universal.css";
+import "../stylesheets/home-menu.css";
+import "../stylesheets/icon-select-menu.css";
+import "../stylesheets/ship-placement-menu.css";
