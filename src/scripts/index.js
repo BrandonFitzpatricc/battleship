df7cb0a8f677ac07c969cb83815a5af67c0494d2
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-06-29 10:56:58 -0400

    feat: write game screen page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,4 @@ import "../stylesheets/universal.css";
 import "../stylesheets/home-menu.css";
 import "../stylesheets/icon-select-menu.css";
 import "../stylesheets/ship-placement-menu.css";
+import "../stylesheets/game-screen.css";
