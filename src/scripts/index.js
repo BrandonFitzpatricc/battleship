e9bb4d51db0f6dd03286130a7a5f4a6e8d8e5151
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-06-30 10:18:51 -0400

    feat: generalize the game over prompt stylesheet

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,6 @@ import "../stylesheets/home-menu.css";
 import "../stylesheets/icon-select-menu.css";
 import "../stylesheets/ship-placement-menu.css";
 import "../stylesheets/game-screen.css";
-import "../stylesheets/game-over-prompt.css";
+import "../stylesheets/prompt.css";
 
 document.querySelector("#game-over-prompt").showModal();
