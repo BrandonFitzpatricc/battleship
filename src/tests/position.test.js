03abb83eb9aa38cec59396f11356b9939645d57e
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-01 13:31:27 -0400

    test: write position object instantiation test

---
+++ src/tests/position.test.js
@@ -0,0 +1,9 @@
+import { Position } from "../scripts/model/position";
+
+describe("Test position object instantiation", () => {
+  test("Position objects are successfully created with the specified row and column values", () => {
+    const position = new Position(4, 3);
+    expect(position.row).toBe(4);
+    expect(position.column).toBe(3);
+  });
+});
