30a04b549eb6c409ed2c8cb8c730b4a67d182fb7
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-01 13:16:19 -0400

    feat: write GameBoard class

--- src/scripts/model/game-board.js
+++ src/scripts/model/game-board.js
@@ -1,10 +1,15 @@
 import { Ship } from "./ship.js";
+import { Position } from "./position.js";
 
 class GameBoard {
   #board;
   #ships;
 
   constructor() {
+    this.clearBoard();
+  }
+
+  clearBoard() {
     this.#board = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,19 +26,92 @@ class GameBoard {
     this.#ships = [];
   }
 
-  // headPosition is the leftmost ship position if the orientation is horizontal, or the topmost
-  // ship position if the orientation is vertical
-  placeShip(name, headPosition, orientation) {
-    const ship = new Ship(name);
-    this.#ships.push(ship);
-  }
-
   get board() {
     return this.#board;
   }
 
   get ships() {
     return this.#ships;
+  }
+
+  placeShipHorizontally(name, headPosition) {
+    return this.#placeShip(name, headPosition, "horizontal");
+  }
+
+  placeShipVertically(name, headPosition) {
+    return this.#placeShip(name, headPosition, "vertical");
+  }
+
+  // headPosition is the leftmost ship position if the orientation is horizontal, or the topmost
+  // ship position if the orientation is vertical
+  #placeShip(name, headPosition, orientation) {
+    const ship = new Ship(name);
+    if (this.#invalidPlacement(ship, headPosition, orientation)) return false;
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === "horizontal") {
+        this.#board[headPosition.row][headPosition.column + i] = ship;
+        //prettier-ignore
+      } else if (orientation === "vertical") {
+        this.#board[headPosition.row + i][headPosition.column] = ship;
+      }
+    }
+    this.#ships.push(ship);
+    return true;
+  }
+
+  #invalidPlacement(ship, headPosition, orientation) {
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === "horizontal") {
+        const invalidPlacement =
+          headPosition.column + ship.length - 1 > 9 ||
+          typeof this.#board[headPosition.row][headPosition.column + i] ===
+            "object";
+        if (invalidPlacement) return true;
+        //prettier-ignore
+      } else if (orientation === "vertical") {
+        const invalidPlacement =
+          headPosition.row + ship.length - 1 > 9 ||
+          typeof this.#board[headPosition.row + i][headPosition.column] ===
+            "object";
+        if (invalidPlacement) return true;
+      }
+    }
+    return false;
+  }
+
+  placeShipsRandomly() {
+    this.clearBoard();
+    ["carrier", "battleship", "destroyer", "submarine", "patrol-boat"].forEach(
+      (shipName) => {
+        this.placeShipFn =
+          Math.round(Math.random() * 1) === 0
+            ? this.placeShipHorizontally
+            : this.placeShipVertically;
+
+        while (true) {
+          const row = Math.floor(Math.random() * 10);
+          const column = Math.floor(Math.random() * 10);
+          if (this.placeShipFn(shipName, new Position(row, column))) break;
+        }
+      },
+    );
+  }
+
+  // A position with no ship that has been targeted will be marked as 1
+  receiveAttack(position) {
+    const target = this.#board[position.row][position.column];
+    if (target === 1) return false;
+
+    if (target === 0) {
+      this.#board[position.row][position.column] = 1;
+    } else if (typeof target === "object") {
+      target.hit();
+    }
+    return true;
+  }
+
+  allShipsSunk() {
+    return this.#ships.filter((ship) => ship.isSunk()).length === 5;
   }
 }
 
--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -1,26 +1,233 @@
 import { GameBoard } from "../scripts/model/game-board.js";
+import { Position } from "../scripts/model/position.js";
 
 let gameBoard;
 beforeEach(() => {
   gameBoard = new GameBoard();
 });
 
-test("A game board object is successfully created", () => {
-  expect(gameBoard.board).toEqual([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+describe("Test game board object instantiation", () => {
+  test("A game board object is successfully created", () => {
+    expect(gameBoard.board).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
 });
 
-test("A ship that's placed on the game board is successfully saved", () => {
-  gameBoard.placeShip("battleship", [0, 0], "horizontal");
-  expect(gameBoard.ships[0].name).toBe("battleship");
+describe("Test placeShip method", () => {
+  test("A ship that's placed on the game board is successfully saved", () => {
+    gameBoard.placeShipHorizontally("battleship", new Position(0, 0));
+    expect(gameBoard.ships[0].name).toBe("battleship");
+  });
+
+  test("Placing a ship successfully will return true", () => {
+    //prettier-ignore
+    expect(gameBoard.placeShipHorizontally("carrier", new Position(0, 5))).toBe(true);
+    // prettier-ignore
+    expect(gameBoard.placeShipVertically("battleship", new Position(6, 0))).toBe(true);
+  });
+
+  test("An attempt to place a ship in the same position as another ship will return false", () => {
+    // prettier-ignore
+    expect(gameBoard.placeShipHorizontally("battleship", new Position(1, 4))).toBe(true);
+    // prettier-ignore
+    expect(gameBoard.placeShipVertically("carrier", new Position(0, 5))).toBe(false);
+  });
+
+  test("An attempt to place a ship out of bounds will return false", () => {
+    // prettier-ignore
+    expect(gameBoard.placeShipHorizontally("carrier", new Position(0, 6))).toBe(false);
+    // prettier-ignore
+    expect(gameBoard.placeShipVertically("battleship", new Position(7, 0))).toBe(false);
+  });
+});
+
+describe("Test placeShip + receiveAttack methods", () => {
+  test("A battleship placed horizontally at [0, 0] is sunk after [0, 0], [0, 1], [0, 2], and [0, 3] are targeted", () => {
+    gameBoard.placeShipHorizontally("battleship", new Position(0, 0));
+    gameBoard.receiveAttack(new Position(0, 0));
+    gameBoard.receiveAttack(new Position(0, 1));
+    gameBoard.receiveAttack(new Position(0, 2));
+    gameBoard.receiveAttack(new Position(0, 3));
+    expect(gameBoard.ships[0].isSunk()).toBe(true);
+  });
+
+  test("A battleship placed vertically at [0, 0] is sunk after [0, 0], [1, 0], [2, 0] and [3, 0] are targeted", () => {
+    gameBoard.placeShipVertically("battleship", new Position(0, 0));
+    gameBoard.receiveAttack(new Position(0, 0));
+    gameBoard.receiveAttack(new Position(1, 0));
+    gameBoard.receiveAttack(new Position(2, 0));
+    gameBoard.receiveAttack(new Position(3, 0));
+    expect(gameBoard.ships[0].isSunk()).toBe(true);
+  });
+
+  test("A battleship placed horizontally at [0, 0] is not sunk after [0, 0], [1, 0], [2, 0] and [3, 0] are targeted", () => {
+    gameBoard.placeShipHorizontally("battleship", new Position(0, 0));
+    gameBoard.receiveAttack(new Position(0, 0));
+    gameBoard.receiveAttack(new Position(1, 0));
+    gameBoard.receiveAttack(new Position(2, 0));
+    gameBoard.receiveAttack(new Position(3, 0));
+    expect(gameBoard.ships[0].isSunk()).toBe(false);
+  });
+
+  test("An attack at positions with no ship will change their values to 1", () => {
+    gameBoard.receiveAttack(new Position(0, 0));
+    gameBoard.receiveAttack(new Position(4, 5));
+    gameBoard.receiveAttack(new Position(7, 2));
+    expect(gameBoard.board).toEqual([
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  test("An attack at a position that has already been attacked will return false", () => {
+    expect(gameBoard.receiveAttack(new Position(0, 0))).toBe(true);
+    expect(gameBoard.receiveAttack(new Position(0, 0))).toBe(false);
+  });
+
+  test("Ship placement and attacking works as expected with multiple ships on the board", () => {
+    gameBoard.placeShipVertically("battleship", new Position(2, 1));
+    gameBoard.placeShipVertically("carrier", new Position(2, 7));
+    gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+    gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+    gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+    gameBoard.receiveAttack(new Position(2, 1));
+    gameBoard.receiveAttack(new Position(3, 1));
+    gameBoard.receiveAttack(new Position(4, 1));
+    gameBoard.receiveAttack(new Position(5, 1));
+
+    gameBoard.receiveAttack(new Position(8, 3));
+    gameBoard.receiveAttack(new Position(8, 4));
+    gameBoard.receiveAttack(new Position(8, 5));
+
+    gameBoard.receiveAttack(new Position(3, 7));
+    gameBoard.receiveAttack(new Position(9, 7));
+    gameBoard.receiveAttack(new Position(0, 6));
+
+    gameBoard.receiveAttack(new Position(5, 2));
+    gameBoard.receiveAttack(new Position(2, 0));
+
+    expect(
+      gameBoard.ships.filter((ship) => ship.isSunk()).map((ship) => ship.name),
+    ).toEqual(["battleship", "destroyer"]);
+  });
+});
+
+describe("Test clearBoard method", () => {
+  test("clearBoard method successfully resets a populated game board and ships array", () => {
+    gameBoard.placeShipVertically("battleship", new Position(2, 1));
+    gameBoard.placeShipVertically("carrier", new Position(2, 7));
+    gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+    gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+    gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+    gameBoard.clearBoard();
+    expect(gameBoard.board).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    expect(gameBoard.ships).toEqual([]);
+  });
+});
+
+describe("Test placeShipsRandomly method", () => {
+  test("placeShipsRandomly method successfully places all five ships on the board", () => {
+    gameBoard.placeShipsRandomly();
+
+    let shipSquares = 0;
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      for (let j = 0; j < gameBoard.board[i].length; j++) {
+        if (typeof gameBoard.board[i][j] === "object") shipSquares++;
+      }
+    }
+
+    expect(shipSquares).toBe(17);
+    expect(gameBoard.ships.length).toBe(5);
+  });
+});
+
+describe("Test isSunk method", () => {
+  test("isSunk returns false when there are still active ships", () => {
+    gameBoard.placeShipVertically("battleship", new Position(2, 1));
+    gameBoard.placeShipVertically("carrier", new Position(2, 7));
+    gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+    gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+    gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+    gameBoard.receiveAttack(new Position(2, 1));
+    gameBoard.receiveAttack(new Position(3, 1));
+    gameBoard.receiveAttack(new Position(4, 1));
+    gameBoard.receiveAttack(new Position(5, 1));
+
+    gameBoard.receiveAttack(new Position(8, 3));
+    gameBoard.receiveAttack(new Position(8, 4));
+    gameBoard.receiveAttack(new Position(8, 5));
+
+    gameBoard.receiveAttack(new Position(3, 7));
+    gameBoard.receiveAttack(new Position(9, 7));
+    gameBoard.receiveAttack(new Position(0, 6));
+
+    gameBoard.receiveAttack(new Position(5, 2));
+    gameBoard.receiveAttack(new Position(2, 0));
+
+    expect(gameBoard.allShipsSunk()).toBe(false);
+  });
+
+  test("isSunk returns true when every ship has been sunk", () => {
+    gameBoard.placeShipVertically("battleship", new Position(2, 1));
+    gameBoard.placeShipVertically("carrier", new Position(2, 7));
+    gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+    gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+    gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+    gameBoard.receiveAttack(new Position(2, 1));
+    gameBoard.receiveAttack(new Position(3, 1));
+    gameBoard.receiveAttack(new Position(4, 1));
+    gameBoard.receiveAttack(new Position(5, 1));
+
+    gameBoard.receiveAttack(new Position(2, 7));
+    gameBoard.receiveAttack(new Position(3, 7));
+    gameBoard.receiveAttack(new Position(4, 7));
+    gameBoard.receiveAttack(new Position(5, 7));
+    gameBoard.receiveAttack(new Position(6, 7));
+
+    gameBoard.receiveAttack(new Position(8, 3));
+    gameBoard.receiveAttack(new Position(8, 4));
+    gameBoard.receiveAttack(new Position(8, 5));
+
+    gameBoard.receiveAttack(new Position(0, 3));
+    gameBoard.receiveAttack(new Position(0, 4));
+    gameBoard.receiveAttack(new Position(0, 5));
+
+    gameBoard.receiveAttack(new Position(9, 7));
+    gameBoard.receiveAttack(new Position(9, 8));
+
+    expect(gameBoard.allShipsSunk()).toBe(true);
+  });
 });
