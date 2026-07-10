038a1c5d42edacb3a6066ccf0e0997f485d3cd0c
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-10 14:46:35 -0400

    feat(computer-player): write computer player attack algorithm

--- src/scripts/model/player.js
+++ src/scripts/model/player.js
@@ -1,3 +1,6 @@
+import { Position } from "./position.js";
+import { SuccessfulAttack } from "./successful-attack.js";
+
 class Player {
   #gameBoard;
 
@@ -17,8 +20,248 @@ class RealPlayer extends Player {
 }
 
 class ComputerPlayer extends Player {
+  // This array will function similarly to a queue. When the computer player hits a ship for
+  // the first time, it will be pushed into the queue. The computer player will target
+  // the first ship in the queue until it is destroyed, then that ship will be dequeued.
+  // This ensures that if the computer player hits a new ship while in the middle of targeting
+  // another, each of the two ships will be destroyed one at a time.
+  #damagedShips = [];
+
   constructor(gameBoard) {
     super(gameBoard);
+  }
+
+  attack(gameBoard) {
+    const targetShip = this.#damagedShips[0];
+
+    // This algorithm uses a probability map to determine which position is most likely to
+    // contain a ship, then attacks that position.
+    let probabilityMap;
+
+    // prettier-ignore
+    if (this.#damagedShips.length > 0 && targetShip.previousSuccessfulAttack.orientation) {
+      probabilityMap =
+        targetShip.previousSuccessfulAttack.orientation === "horizontal"
+          ? this.#getHorizontalProbabilityMap(gameBoard)
+          : this.#getVerticalProbabilityMap(gameBoard);
+    } else {
+      probabilityMap = this.#getProbabilityMap(gameBoard);
+    }
+
+    let highestProbability = 0;
+    let attackRow = 0;
+    let attackColumn = 0;
+    let attackOrientation;
+
+    if (this.#damagedShips.length === 0) {
+      for (let i = 0; i < probabilityMap.length; i++) {
+        for (let j = 0; j < probabilityMap[i].length; j++) {
+          if (probabilityMap[i][j] > highestProbability) {
+            highestProbability = probabilityMap[i][j];
+            attackRow = i;
+            attackColumn = j;
+          }
+        }
+      }
+      // Once a ship has been hit for the first time, the algorithm will first check the probability
+      // of every adjacent position and attack the one with the highest probability. This will
+      // repeat until a second hit has been made.
+
+      // Once a ship has been hit for a second time, the algorithm will lock its orientation
+      // and check the probabilities of the closest possible positions in each direction, attacking
+      // the position with the highest probability. This will repeat until the ship
+      // has been sunk.
+    } else {
+      for (let i = -1; i <= 1; i += 2) {
+        const previousSuccessfulAttack = targetShip.previousSuccessfulAttack;
+
+        // prettier-ignore
+        if (previousSuccessfulAttack.orientation !== "vertical") {
+          checkTargetProbability("column", targetShip, i);
+        }
+
+        // prettier-ignore
+        if (previousSuccessfulAttack.orientation !== "horizontal") {
+          checkTargetProbability("row", targetShip, i);
+        }
+      }
+    }
+
+    if (gameBoard.receiveAttack(attackRow, attackColumn)) {
+      if (gameBoard.board[attackRow][attackColumn].length > 1) {
+        const hitShip = gameBoard.board[attackRow][attackColumn][1];
+
+        const damagedShip = this.#damagedShips.find(
+          (ship) => ship.name === hitShip.name,
+        );
+
+        // prettier-ignore
+        if (hitShip.isSunk()) {
+          this.#damagedShips.splice(this.#damagedShips.indexOf(damagedShip), 1);
+          
+        } else if (!damagedShip) {
+          this.#damagedShips.push({
+          name: hitShip.name,
+          previousSuccessfulAttack: new SuccessfulAttack(
+            new Position(attackRow, attackColumn),
+          ),
+          failedAttackPositions: []
+        });
+
+        } else {
+          damagedShip.previousSuccessfulAttack = new SuccessfulAttack(
+            new Position(attackRow, attackColumn),
+            attackOrientation,
+          );
+        }
+
+        if (!gameBoard.allShipsSunk()) {
+          this.attack(gameBoard);
+        }
+      }
+    } else {
+      // This is a safeguard to prevent unique cases where the computer gets stuck repeatedly
+      // attacking a position where an attack has failed while targeting a ship.
+      targetShip.failedAttackPositions.push(
+        new Position(attackRow, attackColumn),
+      );
+    }
+
+    function checkTargetProbability(type, targetShip, offset) {
+      const initialOffset = offset;
+
+      let target, targetRow, targetColumn;
+
+      do {
+        targetRow =
+          type === "row"
+            ? targetShip.previousSuccessfulAttack.position.row + offset
+            : targetShip.previousSuccessfulAttack.position.row;
+
+        targetColumn =
+          type === "column"
+            ? targetShip.previousSuccessfulAttack.position.column + offset
+            : targetShip.previousSuccessfulAttack.position.column;
+
+        const outOfBounds =
+          targetRow > 9 ||
+          targetRow < 0 ||
+          targetColumn > 9 ||
+          targetColumn < 0;
+
+        if (outOfBounds) return;
+
+        target = gameBoard.board[targetRow][targetColumn];
+
+        if (target[1]) {
+          if (target[1].name !== targetShip.name) break;
+        }
+
+        offset = initialOffset < 0 ? offset - 1 : offset + 1;
+      } while (target[0] === 1 && target[1]);
+
+      const targetProbability = probabilityMap[targetRow][targetColumn];
+
+      if (targetProbability > highestProbability) {
+        if (targetShip.failedAttackPositions.length > 0) {
+          for (let position of targetShip.failedAttackPositions) {
+            if (
+              JSON.stringify([targetRow, targetColumn]) ===
+              JSON.stringify(position.getPosition())
+            ) {
+              return;
+            }
+          }
+        }
+
+        attackRow = targetRow;
+        attackColumn = targetColumn;
+
+        highestProbability = probabilityMap[attackRow][attackColumn];
+        attackOrientation = type === "column" ? "horizontal" : "vertical";
+      }
+    }
+  }
+
+  #getHorizontalProbabilityMap(gameBoard) {
+    return this.#getProbabilityMap(gameBoard, "horizontal");
+  }
+
+  #getVerticalProbabilityMap(gameBoard) {
+    return this.#getProbabilityMap(gameBoard, "vertical");
+  }
+
+  #getProbabilityMap(gameBoard, orientation) {
+    const probabilityMap = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    const board = gameBoard.board;
+
+    const remainingShips = gameBoard.ships.filter((ship) => !ship.isSunk());
+    remainingShips.forEach((ship) => {
+      for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+          const headPosition = new Position(i, j);
+          //prettier-ignore
+          if (this.#couldBePlacedHorizontally(board, ship, headPosition) && orientation !== "vertical") {
+            for (let k = 0; k < ship.length; k++) {
+              probabilityMap[headPosition.row][headPosition.column + k]++;
+            }
+          }
+
+          //prettier-ignore
+          if (this.#couldBePlacedVertically(board, ship, headPosition) && orientation !== "horizontal") {
+            for (let k = 0; k < ship.length; k++) {
+              probabilityMap[headPosition.row + k][headPosition.column]++;
+            }
+          }
+        }
+      }
+    });
+    return probabilityMap;
+  }
+
+  #couldBePlacedHorizontally(board, ship, headPosition) {
+    return this.#couldBePlaced(board, ship, headPosition, "horizontal");
+  }
+
+  #couldBePlacedVertically(board, ship, headPosition) {
+    return this.#couldBePlaced(board, ship, headPosition, "vertical");
+  }
+
+  #couldBePlaced(board, ship, headPosition, orientation) {
+    for (let i = 0; i < ship.length; i++) {
+      let currentPosition;
+      let couldNotBePlaced;
+
+      if (orientation === "horizontal") {
+        currentPosition = board[headPosition.row][headPosition.column + i];
+        couldNotBePlaced =
+          (currentPosition[0] === 1 && currentPosition.length === 1) ||
+          headPosition.column + ship.length - 1 > 9;
+      } else if (orientation === "vertical") {
+        currentPosition = board[headPosition.row + i][headPosition.column];
+        couldNotBePlaced =
+          (currentPosition[0] === 1 && currentPosition.length === 1) ||
+          headPosition.row + ship.length - 1 > 9;
+      }
+
+      if (!couldNotBePlaced && currentPosition.length > 1) {
+        couldNotBePlaced = currentPosition[1].isSunk();
+      }
+
+      if (couldNotBePlaced) return false;
+    }
+    return true;
   }
 }
 
--- src/tests/model/player.test.js
+++ src/tests/model/player.test.js
@@ -1,36 +1,323 @@
 import { RealPlayer, ComputerPlayer } from "../../scripts/model/player.js";
 import { GameBoard } from "../../scripts/model/game-board.js";
+import { Position } from "../../scripts/model/position.js";
 
-describe("Test player object instantiation", () => {
+let realPlayer;
+let computerPlayer;
+beforeEach(() => {
+  realPlayer = new RealPlayer(new GameBoard());
+  computerPlayer = new ComputerPlayer(new GameBoard());
+});
+
+describe("test player object instantiation", () => {
   test("Real player objects are successfully created", () => {
-    const player = new RealPlayer(new GameBoard());
-    expect(player.gameBoard.board).toEqual([
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expect(realPlayer.gameBoard.board).toEqual([
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
     ]);
   });
 
   test("Computer player objects are successfully created", () => {
-    const player = new ComputerPlayer(new GameBoard());
-    expect(player.gameBoard.board).toEqual([
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    expect(computerPlayer.gameBoard.board).toEqual([
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
+      [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]],
     ]);
+  });
+});
+
+describe("test attack methods of the computer player object", () => {
+  describe("test regular attacks", () => {
+    beforeEach(() => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(2, 1));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(2, 7));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+    });
+
+    test("The computer's first attack against a new game board is in the center", () => {
+      computerPlayer.attack(realPlayer.gameBoard);
+
+      const realPlayerBoard = realPlayer.gameBoard.board;
+
+      const centerAttack = realPlayerBoard[4][4][0] === 1;
+      realPlayerBoard[4][5][0] === 1 ||
+        realPlayerBoard[5][4][0] === 1 ||
+        realPlayerBoard[5][5][0] === 1;
+
+      expect(centerAttack).toBe(true);
+    });
+
+    test("If [4, 4] has been targeted with a missed attack, then the computer's next attack will be at [5, 5]", () => {
+      realPlayer.gameBoard.receiveAttack(4, 4);
+      computerPlayer.attack(realPlayer.gameBoard);
+      expect(realPlayer.gameBoard.board[5][5][0]).toBe(1);
+    });
+
+    test("If [6, 6], [7, 2], [2, 4], and [5, 3] have been targeted with missed attacks, then the computer's next attack will be at [4, 5]", () => {
+      realPlayer.gameBoard.receiveAttack(6, 6);
+      realPlayer.gameBoard.receiveAttack(7, 2);
+      realPlayer.gameBoard.receiveAttack(2, 4);
+      realPlayer.gameBoard.receiveAttack(5, 3);
+
+      computerPlayer.attack(realPlayer.gameBoard);
+
+      expect(realPlayer.gameBoard.board[4][5][0]).toBe(1);
+    });
+
+    test("If [2, 2], [7, 7], [5, 3], [4, 5], [7, 4], and [3, 6] have been targeted with missed attacks, then the computer's next attack will be at [6, 2]", () => {
+      realPlayer.gameBoard.receiveAttack(2, 2);
+      realPlayer.gameBoard.receiveAttack(7, 7);
+      realPlayer.gameBoard.receiveAttack(5, 3);
+      realPlayer.gameBoard.receiveAttack(4, 5);
+      realPlayer.gameBoard.receiveAttack(7, 4);
+      realPlayer.gameBoard.receiveAttack(3, 6);
+
+      computerPlayer.attack(realPlayer.gameBoard);
+
+      expect(realPlayer.gameBoard.board[6][2][0]).toBe(1);
+    });
+
+    test("The computer does not target the position of a sunk ship", () => {
+      realPlayer.gameBoard.clearBoard();
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("battleship", new Position(4, 4));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(2, 7));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("destroyer", new Position(2, 2));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+      realPlayer.gameBoard.receiveAttack(4, 4);
+      realPlayer.gameBoard.receiveAttack(4, 5);
+      realPlayer.gameBoard.receiveAttack(4, 6);
+      realPlayer.gameBoard.receiveAttack(4, 7);
+
+      realPlayer.gameBoard.receiveAttack(2, 2);
+      realPlayer.gameBoard.receiveAttack(2, 3);
+      realPlayer.gameBoard.receiveAttack(2, 4);
+
+      computerPlayer.attack(realPlayer.gameBoard);
+
+      expect(realPlayer.gameBoard.board[5][2][0]).toBe(1);
+    });
+  });
+
+  describe("test attacks against damaged ships", () => {
+    beforeEach(() => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(2, 9));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("destroyer", new Position(9, 3));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+
+      realPlayer.gameBoard.receiveAttack(1, 1);
+      realPlayer.gameBoard.receiveAttack(2, 5);
+      realPlayer.gameBoard.receiveAttack(8, 6);
+      realPlayer.gameBoard.receiveAttack(0, 6);
+      realPlayer.gameBoard.receiveAttack(3, 0);
+      realPlayer.gameBoard.receiveAttack(6, 8);
+      realPlayer.gameBoard.receiveAttack(4, 3);
+    });
+
+    test("Once a ship has been found, the following move will target one of its adjacent positions", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(5, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      computerPlayer.attack(realPlayer.gameBoard);
+
+      const realPlayerBoard = realPlayer.gameBoard.board;
+
+      const adjacentAttack =
+        realPlayerBoard[4][4][0] === 1 ||
+        realPlayerBoard[5][3][0] === 1 ||
+        realPlayerBoard[5][5][0] === 1 ||
+        realPlayerBoard[6][4][0] === 1;
+
+      expect(adjacentAttack).toBe(true);
+    });
+
+    test("Once a horizontally placed ship's head has been found, it will be destroyed in no more than four turns", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("battleship", new Position(5, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      let turns = 0;
+      while (true) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+        //prettier-ignore
+        if(realPlayer.gameBoard.ships.find(ship => ship.name === "battleship").isSunk()) break;
+      }
+      expect(turns).toBeLessThanOrEqual(4);
+    });
+
+    test("Once a vertically placed ship's head has been found, it will be destroyed in no more than four turns", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(5, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      let turns = 0;
+      while (true) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+        //prettier-ignore
+        if(realPlayer.gameBoard.ships.find(ship => ship.name === "battleship").isSunk()) break;
+      }
+      expect(turns).toBeLessThanOrEqual(4);
+    });
+
+    test("Once a horizontally placed ship's body has been found, it will be destroyed in no more than four turns", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("battleship", new Position(5, 3));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      let turns = 0;
+      while (true) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+        //prettier-ignore
+        if(realPlayer.gameBoard.ships.find(ship => ship.name === "battleship").isSunk()) break;
+      }
+      expect(turns).toBeLessThanOrEqual(4);
+    });
+
+    test("Once a vertically placed ship's body has been found, it will be destroyed in no more than four turns", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(4, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      let turns = 0;
+      while (true) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+        //prettier-ignore
+        if(realPlayer.gameBoard.ships.find(ship => ship.name === "battleship").isSunk()) break;
+      }
+      expect(turns).toBeLessThanOrEqual(4);
+    });
+
+    test("If a new ship is hit while targeting another ship, then both ships will be destroyed in no more than eight turns", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(4, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(3, 4));
+
+      let turns = 0;
+      while (turns < 3) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+        if (
+          realPlayer.gameBoard.ships
+            .find((ship) => ship.name === "battleship")
+            .isSunk() &&
+          realPlayer.gameBoard.ships
+            .find((ship) => ship.name === "submarine")
+            .isSunk()
+        )
+          break;
+      }
+      expect(turns).toBeLessThanOrEqual(8);
+    });
+  });
+
+  describe("test computer attack stability", () => {
+    test("The computer is able to destroy every ship in a game board of spread out ships", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(2, 9));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("destroyer", new Position(9, 3));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("battleship", new Position(5, 4));
+      //prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+
+      let turns = 0;
+      while (!realPlayer.gameBoard.allShipsSunk()) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+      }
+    });
+
+    test("The computer is able to destroy every ship in a game board where every ship is placed directly next to each other in the top left corner", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(0, 0));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("battleship", new Position(0, 1));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("destroyer", new Position(1, 1));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(1, 2));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("patrol-boat", new Position(2, 2));
+
+      let turns = 0;
+      while (!realPlayer.gameBoard.allShipsSunk()) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+      }
+    });
+
+    test("The computer is able to destroy every ship in a game board where every ship is placed in all four corners", () => {
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("carrier", new Position(0, 0));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("battleship", new Position(0, 6));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("destroyer", new Position(7, 9));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipHorizontally("submarine", new Position(9, 0));
+      // prettier-ignore
+      realPlayer.gameBoard.placeShipVertically("patrol-boat", new Position(1, 9));
+
+      let turns = 0;
+      while (!realPlayer.gameBoard.allShipsSunk()) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+      }
+    });
+
+    test("The computer is able to destroy every ship in a game board with randomized ship placements", () => {
+      realPlayer.gameBoard.placeShipsRandomly();
+
+      let turns = 0;
+      while (!realPlayer.gameBoard.allShipsSunk()) {
+        turns++;
+        computerPlayer.attack(realPlayer.gameBoard);
+      }
+    });
   });
 });
