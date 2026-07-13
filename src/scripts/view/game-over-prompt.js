b7c69ebec524b6a59ba923b2735c55fc06c3120f
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-13 15:41:02 -0400

    feat(ui): write function for loading the game over prompt

---
+++ src/scripts/view/game-over-prompt.js
@@ -0,0 +1,43 @@
+import {
+  createElement,
+  createTextElement,
+  Attribute,
+} from "./element-factory.js";
+
+const loadGameOverPrompt = () => {
+  const gameOverPrompt = createElement("div", "prompt");
+
+  const content = loadContent();
+
+  gameOverPrompt.appendChild(content);
+
+  document.body.append(gameOverPrompt);
+};
+
+function loadContent() {
+  const content = createElement("div", "content");
+
+  const playAgainBtn = createTextElement(
+    "button",
+    "text-btn play-again",
+    "Play Again",
+  );
+
+  const backToMenuBtn = createTextElement(
+    "button",
+    "text-btn back-to-menu",
+    "Back to Menu",
+  );
+
+  const closeBtn = createElement(
+    "button",
+    "close-btn",
+    new Attribute("id", "close-prompt"),
+  );
+
+  content.append(playAgainBtn, backToMenuBtn, closeBtn);
+
+  return content;
+}
+
+export { loadGameOverPrompt };
