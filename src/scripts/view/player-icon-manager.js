84989f8b584764274b5dedc8392d498fb1572f58
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-13 12:31:33 -0400

    feat: move playerIcons object to its own module

---
+++ src/scripts/view/player-icon-manager.js
@@ -0,0 +1,15 @@
+import boxingGlove from "../../icons/player-icons/boxing-glove.svg";
+import butterfly from "../../icons/player-icons/butterfly.svg";
+import brain from "../../icons/player-icons/brain.svg";
+import bullseye from "../../icons/player-icons/bullseye.svg";
+import heart from "../../icons/player-icons/heart.svg";
+
+const playerIcons = {
+  "boxing-glove": { src: boxingGlove, alt: "icon of a boxing glove" },
+  butterfly: { src: butterfly, alt: "icon of a butterfly" },
+  brain: { src: brain, alt: "icon of a brain" },
+  bullseye: { src: bullseye, alt: "icon of a bullseye" },
+  heart: { src: heart, alt: "icon of a heart" },
+};
+
+export { playerIcons };
