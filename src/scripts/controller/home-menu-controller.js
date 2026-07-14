693811a79f716662029fdfcb90b5a3fef7929784
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-14 14:03:10 -0400

    feat: create and pass player objects between controllers

--- src/scripts/controller/home-menu-controller.js
+++ src/scripts/controller/home-menu-controller.js
@@ -1,14 +1,16 @@
-import { loadHomeMenu } from "../view/home-menu.js";
 import { initializeIconSelectMenu } from "./icon-select-menu-controller.js";
+
+import { loadHomeMenu } from "../view/home-menu.js";
+import { Player, ComputerPlayer } from "../model/player.js";
 
 const initializeHomeMenu = () => {
   loadHomeMenu();
 
   document.querySelector(".menu-btns").addEventListener("click", (event) => {
     if (event.target.id === "one-player") {
-      initializeIconSelectMenu();
+      initializeIconSelectMenu([new Player(), new ComputerPlayer()]);
     } else if (event.target.id === "two-player") {
-      initializeIconSelectMenu();
+      initializeIconSelectMenu([new Player(), new Player()]);
     }
   });
 };
