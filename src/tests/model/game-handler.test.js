90268e4c3819a33441668d2b72b776f3e7328de0
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-07-28 10:38:11 -0400

    test(game-handler): write test for two player games

--- src/tests/model/game-handler.test.js
+++ src/tests/model/game-handler.test.js
@@ -4,7 +4,7 @@ import { ComputerPlayer } from "../../scripts/model/player";
 import { GameHandler } from "../../scripts/model/game-handler";
 import { Position } from "../../scripts/model/position";
 
-describe("Test game flow", () => {
+describe("Test one player games", () => {
   beforeEach(() => {
     const realPlayer = new Player();
     const computerPlayer = new ComputerPlayer();
@@ -60,5 +60,58 @@ describe("Test game flow", () => {
     }
     // prettier-ignore
     expect(GameHandler.getAttackingPlayer() instanceof ComputerPlayer).toBe(true);
+  });
+});
+
+describe("Test two player games", () => {
+  beforeEach(() => {
+    const player1 = new Player();
+    const player2 = new Player();
+
+    [player1, player2].forEach((player) => {
+      const gameBoard = new GameBoard();
+      gameBoard.placeShipVertically("battleship", new Position(2, 1));
+      gameBoard.placeShipVertically("carrier", new Position(2, 7));
+      gameBoard.placeShipHorizontally("destroyer", new Position(8, 3));
+      gameBoard.placeShipHorizontally("submarine", new Position(0, 3));
+      gameBoard.placeShipHorizontally("patrol-boat", new Position(9, 7));
+      player.gameBoard = gameBoard;
+    });
+
+    GameHandler.setup([player1, player2]);
+    GameHandler.startNewGame([player1, player2]);
+  });
+
+  test("A full two player game can be completed from start to finish", () => {
+    GameHandler.playRound(2, 1);
+    GameHandler.playRound(2, 2);
+
+    GameHandler.playRound(8, 3);
+    GameHandler.playRound(8, 4);
+    GameHandler.playRound(8, 5);
+    GameHandler.playRound(5, 3);
+
+    GameHandler.playRound(3, 1);
+    GameHandler.playRound(4, 1);
+    GameHandler.playRound(5, 1);
+
+    GameHandler.playRound(2, 7);
+    GameHandler.playRound(3, 7);
+    GameHandler.playRound(4, 7);
+    GameHandler.playRound(5, 7);
+    GameHandler.playRound(6, 7);
+
+    GameHandler.playRound(8, 3);
+    GameHandler.playRound(8, 4);
+    GameHandler.playRound(8, 5);
+
+    GameHandler.playRound(0, 3);
+    GameHandler.playRound(0, 4);
+    GameHandler.playRound(0, 5);
+
+    GameHandler.playRound(9, 7);
+    GameHandler.playRound(9, 8);
+
+    expect(GameHandler.isGameOver()).toBe(true);
   });
 });
